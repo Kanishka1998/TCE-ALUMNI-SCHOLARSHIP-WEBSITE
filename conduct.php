dfdd2ea6d0cf400d09d38e778c6edcf857e50eca
Kanishka1998 <mdkanish@gmail.com>
2017-11-15 17:22:42 +0530

    Add files via upload

---
+++ conduct.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+RegNo:<input type="text" name="regno" >&nbsp &nbsp 
+Conduct:<select name="conduct" required="required">
+  <option value="5">Excellent</option>
+  <option value="4">Very Good</option>
+  <option value="3">Good</option>
+  <option value="2">Satisfactory</option>
+  
+</select>
+Remarks:<textarea name="address" rows="7" cols="60" />
+<br>
+
+</body>
+</html>
